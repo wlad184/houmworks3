39bf0ed8dece58c7c49a6113d544a73882e38461
wlad184 <evel184@yandex.ru>
2022-11-16 17:28:27 +0700

    task #1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
+        //task #1
+        int age = 21;
+        if (age >= 18) {
+            System.out.println("Поздравляем вы достигли совершеннолетия!");
+        }
+        if (age < 18) {
+            System.out.println("Возраст совершеннолетия ещё не наступил, просим вас немного подождать!");
+        }
     }
+
 }
